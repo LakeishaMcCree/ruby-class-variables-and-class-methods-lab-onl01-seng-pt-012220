bf6441ed9f82ca7be8ba19b2beaf3e0bdb16e541
Lakeisha McCree <lmccree215@icloud.com>
2020-02-11 01:37:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,8 +36,17 @@ class Song
       end
     end
     genre_count 
-        
   end
+  
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
 end
   
   
